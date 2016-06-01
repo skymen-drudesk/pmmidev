5ef3bb2a122536dd55c1cc64105d055d5669a41a
Doug Dobrzynski <ddobrzynski@pmmimediagroup.com>
2016-06-01 14:44:33 -0500

    #1395 Creating audience switcher block in audience_select module.

---
+++ html/modules/custom/audience_select/src/Plugin/Block/AudienceSwitcherBlock.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\audience_select\Plugin\Block\AudienceSwitcherBlock.
+ */
+
+namespace Drupal\audience_select\Plugin\Block;
+
+use Drupal\audience_select\Controller\AudienceSelectController;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Block;
+
+/**
+ * Provides a 'Audience Switcher' block.
+ *
+ * @Block(
+ *   id = "audience_switcher",
+ *   admin_label = @Translation("Audience Switcher")
+ * )
+ */
+class AudienceSwitcherBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $audiences = AudienceSelectController::getKeyedAudiences();
+    return array(
+      '#theme' => 'block__audience_switcher',
+      '#audiences' => $audiences,
+    );
+  }
+
+}
