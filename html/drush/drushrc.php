973a3f31737422f041c58023a55bd299d05daf00
Doug Dobrzynski <ddobrzynski@pmmimediagroup.com>
2016-12-06 11:16:42 -0600

    #1243 Refactoring drushrc to define skipped modules for config import and export only once.

--- html/drush/drushrc.php
+++ html/drush/drushrc.php
@@ -3,7 +3,7 @@
  * @file
  */
 
-$command_specific['config-export']['skip-modules'] = [
+$ignore_modules = [
   'config_devel',
   'config_read_only',
   'config_update_ui',
@@ -17,17 +17,6 @@ $command_specific['config-export']['skip-modules'] = [
   'views_ui',
   'yamlform',
 ];
-$command_specific['config-import']['skip-modules'] = [
-  'config_devel',
-  'config_read_only',
-  'config_update_ui',
-  'dblog',
-  'devel',
-  'devel_debug_log',
-  'field_ui',
-  'page_manager',
-  'stage_file_proxy',
-  'syslog',
-  'views_ui',
-  'yamlform',
-];
+
+$command_specific['config-export']['skip-modules'] = $ignore_modules;
+$command_specific['config-import']['skip-modules'] = $ignore_modules;
