a04c3b4823e50453fc647daf80eafef0196026a1
Doug Dobrzynski <ddobrzynski@pmmimediagroup.com>
2017-02-09 14:59:23 -0600

    #2273 Splitting out config around site template and search panels pages and importing during build.

--- build/update.sh
+++ build/update.sh
@@ -24,6 +24,7 @@ echo "Clearing Drush cache."
 $drush cc drush
 echo "Reverting configuration."
 $drush cim sync --partial -y
+$drush cim panels_pages --partial -y
 echo "Enabling modules again to ensure environment specific modules are enabled.";
 $drush en $(echo $DROPSHIP_SEEDS | tr ':' ' ') -y
 if [ "$SITE_ENVIRONMENT" = "test" ]; then
