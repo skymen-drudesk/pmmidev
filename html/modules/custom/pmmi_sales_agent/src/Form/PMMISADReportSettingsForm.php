846c7d737eced093a168a3c1e1e178b47de74666
alan-ps <s.puchkovskii@internetdevels.com>
2017-03-14 13:21:19 +0200

    2435 - Implement batch page for favourites export:
    Override base batch controller for favorites.
    
    Add update function to set image header.
    
    Add theming to the batch page.

--- html/modules/custom/pmmi_sales_agent/src/Form/PMMISADReportSettingsForm.php
+++ html/modules/custom/pmmi_sales_agent/src/Form/PMMISADReportSettingsForm.php
@@ -4,6 +4,7 @@ namespace Drupal\pmmi_sales_agent\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\block_content\Entity\BlockContent;
 
 /**
  * Provide reporting settings form.
@@ -50,6 +51,22 @@ class PMMISADReportSettingsForm extends ConfigFormBase {
       '#description' => $this->t('A message which is appear if download limit has been exceeded.'),
       '#required' => TRUE,
     ];
+    $form['download_favorites']['img_header'] = [
+      '#type' => 'entity_autocomplete',
+      '#title' => $this->t('Image header'),
+      '#description' => $this->t('Select an image header for the favorites progress bar page.'),
+      '#target_type' => 'block_content',
+      '#selection_handler' => 'default:block_content',
+      '#selection_settings' => [
+        'target_bundles' => ['image_header'],
+      ],
+    ];
+
+    $bid = $config->get('img_header');
+    if ($bid) {
+      $block = BlockContent::load($bid);
+      $form['download_favorites']['img_header']['#default_value'] = $block;
+    }
 
     return $form;
   }
@@ -68,6 +85,7 @@ class PMMISADReportSettingsForm extends ConfigFormBase {
     $this->configFactory()->getEditable('pmmi_sales_agent.reporting_settings')
       ->set('records_per_year', $form_state->getValue('records_per_year'))
       ->set('exceeded_message', $form_state->getValue('exceeded_message'))
+      ->set('img_header', $form_state->getValue('img_header'))
       ->save();
 
     parent::submitForm($form, $form_state);
