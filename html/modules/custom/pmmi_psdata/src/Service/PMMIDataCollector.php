0d2e164127469ec5abc53c9422e7ef7790cd8df3
Oleksandr Chernyi <o.o.chernyi@gmail.com>
2017-03-18 23:00:40 +0200

    1797 Committees page backend
    Updated composer.lock.
    Added PMMICommitteeBlock.
    Added methods to the PMMIDataCollector service: getData, getCommitteeData, getMemberJobTitle, sort, formatDate, buildGetRequest.
    Added template for the PMMICommitteeBlock.
    Changed core.extension.yml, config_split.config_split.config_panels_pages.yml.
    Added panels page for the committee pages:
    - Board of Directors
    - Business Intelligence Committee
    - Education & Workforce Development Committee
    - Emerging Leaders Committee
    - Global Marketing Committee
    - Industry Relations Committee
    - Membership Committee
    - Show Committee
    - Strategic Planning Committee

--- html/modules/custom/pmmi_psdata/src/Service/PMMIDataCollector.php
+++ html/modules/custom/pmmi_psdata/src/Service/PMMIDataCollector.php
@@ -4,7 +4,6 @@ namespace Drupal\pmmi_psdata\Service;
 
 use Drupal\pmmi_sso\Service\PMMISSOHelper;
 use GuzzleHttp\Client;
-use Drupal\Core\Config\ConfigFactory;
 use Drupal\Core\Cache\DatabaseBackend;
 use GuzzleHttp\Exception\RequestException;
 
@@ -23,18 +22,11 @@ class PMMIDataCollector {
   protected $httpClient;
 
   /**
-   * Drupal\Core\Config\ConfigFactory definition.
-   *
-   * @var \Drupal\Core\Config\ConfigFactory
-   */
-  protected $configFactory;
-
-  /**
    * Drupal\Core\Cache\DatabaseBackend definition.
    *
    * @var \Drupal\Core\Cache\DatabaseBackend
    */
-  protected $cacheDefault;
+  protected $cache;
 
   /**
    * Stores PMMISSO helper.
@@ -44,79 +36,179 @@ class PMMIDataCollector {
   protected $ssoHelper;
 
   /**
-   * Constructor.
+   * PMMIDataCollector constructor.
    *
-   * @param \Drupal\user\UserDataInterface $user_data
-   *   The user data service.
    * @param Client $http_client
    *   The HTTP Client library.
+   * @param \Drupal\Core\Cache\DatabaseBackend $cache_default
+   *   The cache to use for the Personify service data.
    * @param PMMISSOHelper $sso_helper
    *   The PMMI SSO Helper service.
    */
   public function __construct(
     Client $http_client,
-    ConfigFactory $config_factory,
     DatabaseBackend $cache_default,
     PMMISSOHelper $sso_helper
   ) {
     $this->httpClient = $http_client;
-    $this->configFactory = $config_factory;
-    $this->cacheDefault = $cache_default;
+    $this->cache = $cache_default;
     $this->ssoHelper = $sso_helper;
   }
 
+  /**
+   * Get Members Data.
+   *
+   * @param string $data_type
+   *   The type of requested data.
+   * @param string $id
+   *   The ID of requested data.
+   *
+   * @return array
+   *   The array of data.
+   */
+  public function getData($data_type, $id) {
+    $data = NULL;
+    $cid = PMMISSOHelper::PROVIDER . ':' . $data_type . '_' . $id;
+    if ($cache = $this->cache->get($cid)) {
+      return $cache->data;
+    }
+    switch ($data_type) {
+      case 'committee':
+        $data = $this->getCommitteeData($id);
+        break;
+
+      case 'about':
+        $data = $this->getCommitteeData($id);
+        break;
+    }
+    if ($data) {
+      $this->cache->set($cid, $data);
+    }
+    return $data;
+  }
 
   /**
-   * Get User Data that just registered via PMMI SSO.
+   * Get Committee Data.
    *
-   * @param string $personify_id
-   *   The Personify user ID.
-   * @param User $account
-   *   The user account.
-   * @param array $user_data
-   *   The array of last update timestamp.
+   * @param string $id
+   *   The ID of requested committee.
+   *
+   * @return array
+   *   The array of data.
    */
-  public function getServiceData($page_type, $id) {
+  protected function getCommitteeData($id) {
+    $data = NULL;
+    $date = new \DateTime();
+    $query = [
+      '$filter' => "CommitteeMasterCustomer eq '$id' and " .
+        "ParticipationStatusCodeString eq 'ACTIVE' and EndDate ge datetime'" .
+        $date->format('Y-m-d') . "'",
+    ];
+    $request_options = $this->buildGetRequest('CommitteeMembers', $query);
 
-    $request_options = $this->ssoHelper->buildDataServiceQuery(
-      'CustomerInfos',
-      array(
-        '$filter' => "MasterCustomerId eq '$personify_id'",
-        '$select' => "LabelName, FirstName, LastName",
-      )
-    );
-    $request_options['method'] = 'GET';
-    $response = $this->handleRequest($request_options);
-    if ($response instanceof RequestException) {
-      $this->ssoHelper->log("Error with request to get User Data: " . $response->getMessage());
-      return;
-    }
-    // Check if user data exist.
-    if ($json_data = json_decode($response)) {
-      $data = $json_data->d[0];
-      // Parse and set user LabelName.
-      if ($label_name = $data->LabelName) {
-        $label_name == $account->getAccountName() ?: $account->set('name', $label_name);
+    if ($committee_data = $this->handleRequest($request_options)) {
+      foreach ($committee_data as $customer) {
+        $last_first_name = $customer->CommitteeMemberLastFirstName;
+        $member_id = $customer->MemberMasterCustomer;
+        $member_sub_id = $customer->MemberSubCustomer;
+        $position = $customer->PositionCodeDescriptionDisplay;
+
+        $data[$position][$last_first_name] = [
+          'label_name' => $customer->CommitteeMemberLabelName,
+          'end_date' => $this->formatDate($customer->EndDate, 'Y'),
+          'member_id' => $member_id,
+          'company_id' => $customer->RepresentingMasterCustomer,
+          'company_name' => $customer->RepresentingLabelName,
+        ];
+        if ($job_title = $this->getMemberJobTitle($member_id, $member_sub_id)) {
+          $data[$position][$last_first_name]['job_title'] = $job_title;
+        }
+        $this->sort($data);
       }
-      else {
-        $account->block();
-        $this->ssoHelper->log('User name not exist or disabled.');
-        return;
-      }
-      // Parse and set user FirstName.
-      if ($first_name = $data->FirstName) {
-        $first_name == $account->get('field_first_name') ?: $account->set('field_first_name', $first_name);
-      }
-      // Parse and set user LastName.
-      if ($last_name = $data->LastName) {
-        $last_name == $account->get('field_last_name') ?: $account->set('field_last_name', $last_name);
-      }
-      $user_data['info'] = REQUEST_TIME;
     }
     else {
-      $this->ssoHelper->log('Invalid response from SSO Service.');
-      return;
+      $this->ssoHelper->log("Error with request to get Data Service Committee Members.");
     }
+    return $data;
+  }
+
+  /**
+   * Get member Job Title.
+   *
+   * @param string $member_id
+   *   The MemberMasterCustomer ID.
+   * @param int $member_sub_id
+   *   The MemberSubCustomer ID.
+   *
+   * @return string
+   *   The job title.
+   */
+  protected function getMemberJobTitle($member_id, $member_sub_id) {
+    $job_title = '';
+    // Example path: CustomerInfos(MasterCustomerId='00094039',
+    // SubCustomerId=0)/Addresses?$select=JobTitle .
+    $path_element = "CustomerInfos(MasterCustomerId='" . $member_id .
+      "',SubCustomerId=" . $member_sub_id . ")/Addresses";
+    $query = [
+      '$filter' => "AddressStatusCode eq 'GOOD'",
+      '$select' => 'JobTitle',
+    ];
+    $request_options = $this->buildGetRequest($path_element, $query);
+    if ($job_data = $this->handleRequest($request_options)) {
+      empty($job_data[0]->JobTitle) ?: $job_title = $job_data[0]->JobTitle;
+    }
+    return $job_title;
+  }
+
+  /**
+   * Helper function for sorting data.
+   *
+   * @param array $data
+   *   The Data array.
+   *
+   * @return array
+   *   The Data array.
+   */
+  protected function sort(array &$data) {
+    foreach ($data as &$value) {
+      ksort($value);
+    }
+    return ksort($data);
+  }
+
+  /**
+   * Helper function for formatting Json date.
+   *
+   * @param string $json_date
+   *   The Date string.
+   * @param string $format
+   *   The format string for date output.
+   *
+   * @return string
+   *   The formatted date.
+   */
+  protected function formatDate($json_date, $format) {
+    $timestamp = preg_replace('/[^0-9]/', '', $json_date);
+    return date($format, $timestamp / 1000);
+  }
+
+  /**
+   * Helper function for building GET request.
+   *
+   * @param string $collection
+   *   The requested collection/Path.
+   * @param array $query
+   *   The array of query parameters.
+   *
+   * @return array
+   *   The request options array.
+   */
+  protected function buildGetRequest($collection, array $query) {
+    $request_options = $this->ssoHelper->buildDataServiceQuery(
+      $collection, $query
+    );
+    $request_options['timeout'] = $this->ssoHelper->getConnectionTimeout();
+    return $request_options;
   }
 
   /**
@@ -125,25 +217,29 @@ class PMMIDataCollector {
    * @param array $request_param
    *   Parameters of the request.
    *
-   * @return string|RequestException
-   *   The response data from PMMI Personify Services.
+   * @return array
+   *   The JSON decoded array of response from PMMI Personify Services.
    */
   protected function handleRequest(array $request_param) {
-    $method = $request_param['method'];
     $uri = $request_param['uri'];
-    if ($method == 'POST') {
-      $options = ['form_params' => $request_param['params']];
-    }
-    else {
-      $options = $request_param['params'];
-    }
+    $options = $request_param['params'];
+    $data = array();
     try {
-      $response = $this->httpClient->request($method, $uri, $options);
+      $response = $this->httpClient->get($uri, $options);
       $response_data = $response->getBody()->getContents();
-      $this->ssoHelper->log("User Data received from PMMI Personify server: " . htmlspecialchars($response_data));
-    } catch (RequestException $e) {
-      return $e;
+      $this->ssoHelper->log("Response received from PMMI Personify server: " . htmlspecialchars($response_data));
+      if ($json_data = json_decode($response_data)) {
+        $data = $json_data->d;
+      }
+      else {
+        $this->ssoHelper->log("Can't parse Json data from Data Service.");
+      }
     }
-    return $response_data;
+    catch (RequestException $e) {
+      $this->ssoHelper->log('Invalid response from Data Service.');
+      return $data;
+    }
+    return $data;
   }
+
 }
