ee3e7501c53c6d64803d53724c3143f11b9b5250
dd-zen <s.markov@internetdevels.com>
2017-07-20 16:32:52 +0300

    #2686: Changed module. Added new entityt

--- html/modules/custom/pmmi_page_manager_search/src/Entity/PageManagerSearch.php
+++ html/modules/custom/pmmi_page_manager_search/src/Entity/PageManagerSearch.php
@@ -2,20 +2,17 @@
 
 namespace Drupal\pmmi_page_manager_search\Entity;
 
+use Drupal\Core\Annotation\Translation;
+use Drupal\Core\Entity\Annotation\ContentEntityType;
 use Drupal\Core\Entity\ContentEntityBase;
 use Drupal\Core\Entity\ContentEntityInterface;
-use Drupal\Core\Entity\Entity;
-use Drupal\Core\Entity\EntityStorageInterface;
-use Drupal\Core\Entity\EntityType;
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Field\BaseFieldDefinition;
-use Drupal\page_manager\Entity\Page;
-use Drupal\page_manager\Entity\PageVariant;
 
 
 /**
  * Defines the Page manager search entity.
- *  Emulate entity for making integration with search_api module.
+ *  Emulate entity for making Page Variant entity searchable.
  *
  * @ingroup pmmi_page_manager_search
  *
@@ -24,15 +21,35 @@ use Drupal\page_manager\Entity\PageVariant;
  *   label = @Translation("Page Manager Search"),
  *   base_table = "page_manager_search",
  *   entity_keys = {
- *     "id" = "id",
+ *    "id" = "id",
+ *    "label" = "name",
+ *    "uuid" = "uuid"
  *   },
  * )
  */
 
-// *   handlers = {
-// *     "storage" = "Drupal\pmmi_page_manager_search\PageManagerSearchStorage",
-// *   },
 class PageManagerSearch extends ContentEntityBase implements ContentEntityInterface {
+
+  /**
+   * Get referenced Page Variant Entity.
+   */
+  public function getPageVariant() {
+    return $this->get('pid')->entity;
+  }
+
+  /**
+   * Get referenced Page Variant Id.
+   */
+  public function getPageVariantId() {
+    return $this->get('pid')->target_id;
+  }
+
+  /**
+   * Get referenced Page Variant Id.
+   */
+  public function setPageVariantId($pid) {
+    return $this->set('pid', $pid);
+  }
 
   /**
    * {@inheritdoc}
@@ -74,7 +91,7 @@ class PageManagerSearch extends ContentEntityBase implements ContentEntityInterf
 
     $fields['pid'] = BaseFieldDefinition::create('entity_reference')
       ->setLabel(t('Page variant ID'))
-      ->setDescription(t('Page variant ID of the referenced Page'))
+      ->setDescription(t('Page variant ID of the referenced Page.'))
       ->setSetting('target_type', 'page_variant')
       ->setSetting('handler', 'default')
       ->setRequired(TRUE)
@@ -103,77 +120,4 @@ class PageManagerSearch extends ContentEntityBase implements ContentEntityInterf
 
     return $fields;
   }
-//
-//  /**
-//   * {@inheritdoc}
-//   */
-//  public static function load($sid) {
-//    $page_variant = pmmi_page_manager_search_get_pages_by_dec($sid);
-//
-//    $entity = new PageManagerSearch([], 'page_manager_search');
-//    $contexts = $page_variant->getContexts();
-//    $context_check = TRUE;
-//
-//    if (!empty($contexts)) {
-//      $contexts = array_keys($contexts);
-//      if (count($contexts) == 1 && in_array('current_user', $contexts)) {
-//        $context_check = TRUE;
-//      }
-//      else {
-//        $context_check = FALSE;
-//      }
-//    }
-//
-//    if ($page_variant instanceof PageVariant && $context_check === TRUE) {
-//      $page = $page_variant->getPage();
-//      if ($page instanceof Page) {
-//        $entity->pid = $sid;
-//        $render = \Drupal::entityTypeManager()
-//          ->getViewBuilder('page_variant')
-//          ->view($page_variant);
-//        $entity->title = $page->label();
-//
-//        $content = \Drupal::service('renderer')->renderRoot($render);
-//        $content = strip_tags($content);
-//        $content = trim(preg_replace('/\s+/', ' ', $content));
-//
-//        $entity->content = $content;
-//        $entity->path = $page->getPath();
-//      }
-//    }
-//    else {
-//      $entity = NULL;
-//    }
-//
-//    return $entity;
-//  }
-//
-//  /**
-//   * {@inheritdoc}
-//   */
-//  public static function loadMultiple(array $ids = NULL) {
-//    $results = [];
-//
-//    if (empty($ids)) {
-//      $pages = \Drupal::entityTypeManager()
-//        ->getStorage('page_variant')
-//        ->loadMultiple();
-//
-//      foreach ($pages as $page_variant_id => $page_variant) {
-//        $sid = pmmi_page_manager_search_machine_name_to_dec($page_variant_id);
-//
-//        if (!empty($sid)) {
-//          $results[] = PageManagerSearch::load($sid);
-//          unset($pages[$page_variant_id]);
-//        }
-//      }
-//    }
-//    else {
-//      foreach ($ids as $id) {
-//        $results[] = PageManagerSearch::load($id);
-//      }
-//    }
-//
-//    return $results;
-//  }
 }
