c91631c64f7d4ed4f71680de08b5a6946ff74f2d
Dev User <devuser@pmmimediagroup.com>
2018-02-20 20:39:04 +0000

    Temporarily disable memcache as backend.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -716,9 +716,9 @@ if (file_exists('/var/www/site-php')) {
 }
 
 // set memcache as default cache backend.
-if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
-  $settings['cache']['default'] = 'cache.backend.memcache';
-}
+# if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
+  # $settings['cache']['default'] = 'cache.backend.memcache';
+# }
 
 # Disable Drupal Core Search Functionality
 $conf['search_cron_limit'] = 0;
