2989d071ebcf14ec3767b7c7ea0124cddc4b70ea
dsdobrzynski <dsdobrzynski@gmail.com>
2018-09-13 13:17:06 -0500

    Revert "build(composer): actually install packages at versions defined in lock"
    This reverts commit a39d276d978f54f43ea944c773a1d20b69d6bbca.

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit3df454ca224f885138e55feb03d5c79c
+class ComposerStaticInit4b9dc20e2dac857f75fb4bb5fd86632c
 {
     public static $files = array (
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
@@ -1211,10 +1211,10 @@ class ComposerStaticInit3df454ca224f885138e55feb03d5c79c
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit3df454ca224f885138e55feb03d5c79c::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit3df454ca224f885138e55feb03d5c79c::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit3df454ca224f885138e55feb03d5c79c::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInit3df454ca224f885138e55feb03d5c79c::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit4b9dc20e2dac857f75fb4bb5fd86632c::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit4b9dc20e2dac857f75fb4bb5fd86632c::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit4b9dc20e2dac857f75fb4bb5fd86632c::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit4b9dc20e2dac857f75fb4bb5fd86632c::$classMap;
 
         }, null, ClassLoader::class);
     }
